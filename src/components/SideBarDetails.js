e100df7ad803309636d8aeddc94ac0b170a70a02
Zachary Rizer <zacharyrizer@gmail.com>
2020-05-18 08:00:12 -0700

    minor style tweak to side-bar-details

--- src/components/SideBarDetails.js
+++ src/components/SideBarDetails.js
@@ -18,17 +18,29 @@ const SideBarDetails = ({ rover }) => {
         background={`url(/images/${rover}Profile.jpg)`}
       />
       <NavLink to={`/${rover}/image-search`} className="side-bar-navs">
-        <Heading level="4" margin="medium" textAlign="center" rover={rover}>
+        <Heading
+          level="4"
+          margin="medium"
+          textAlign="center"
+          className="side-bar-details">
           Explore Mars With {rover}
         </Heading>
       </NavLink>
       <NavLink to={`/${rover}/rover-details`} className="side-bar-navs">
-        <Heading level="4" margin="medium">
+        <Heading
+          level="4"
+          margin="medium"
+          textAlign="center"
+          className="side-bar-details">
           Rover and Mission Details
         </Heading>
       </NavLink>
       <NavLink to={`/${rover}/iconic-shots`} className="side-bar-navs">
-        <Heading level="4" margin="medium">
+        <Heading
+          level="4"
+          margin="medium"
+          textAlign="center"
+          className="side-bar-details">
           {rover}'s Iconic Shots
         </Heading>
       </NavLink>
